abeaf0b9c7004317e53402c339c982e4ddd32e13
MarkMarley-Dev <markmarley19911@gmail.com>
2021-12-27 12:34:19 +0000

    Updated portfolio section with more projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -25,13 +25,27 @@ const Porfolio = (props) => {
       </div>
       <div className="portfolio-item-two">
         <a href="http://property--analyser.herokuapp.com/">
-          <div className="portfolio-item-one__img" />
+          <div className="portfolio-item-two__img" />
         </a>
         <h2 className="portfolio-item-two__title"> BrainFlix</h2>
         <p className="portfolio-item-two__text">
           BrainFlix is a mock youtube website. Built with dummy data in a server
           with JSON data. I have then used Axios calls to select and switch
           videos. You can also add new videos with a axios post form.
+        </p>
+        <div className="portfolio-logo_ctn"> </div>
+      </div>
+      <div className="portfolio-item-three">
+        <a href="http://property--analyser.herokuapp.com/">
+          <div className="portfolio-item-three__img" />
+        </a>
+        <h2 className="portfolio-item-three__title"> Expenses Tracker</h2>
+        <p className="portfolio-item-three__text">
+          Expenses Tracker is quite a simple but sophisticated React app that
+          data is dynamically added. Here I have worked on passing Data down and
+          bubbling data up through components. I would definitely recommend
+          looking at the code on GitHub as there is some very neat pieces of
+          code that I am proud of.
         </p>
         <div className="portfolio-logo_ctn"> </div>
       </div>
