d45b8ad820241e82b0a68e10eb4306c23c0220cc
MarkMarley-Dev <markmarley19911@gmail.com>
2021-12-27 12:22:07 +0000

    Initial commit

---
+++ src/components/Portfolio.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+const Porfolio = (props) => {
+  return (
+    <section className="portfolio" id="portfolio">
+      <h1 className="portfolio-title">
+        {" "}
+        Please Check out some of my works below.
+      </h1>
+      <div className="portfolio-item-one">
+        <a href="http://property--analyser.herokuapp.com/">
+          <div className="portfolio-item-one__img" />
+        </a>
+        <h2 className="portfolio-item-one__title">Property Analayser</h2>
+        <p className="portfolio-item-one__text">
+          Property Analyser is a tool which helps property investors and
+          homeowners alike search for granular data usually reserved for
+          institutional investors. The project is built in Reactjs with hooks.
+          The data is accessed dynamically through a API and then displayed
+          dynamically through ChartJs and JSX. The styling is done with Sass and
+          the routing done with React-Router-DOM.
+        </p>
+        <div className="portfolio-logo_ctn"> </div>
+      </div>
+      <div className="portfolio-item-two">
+        <a href="http://property--analyser.herokuapp.com/">
+          <div className="portfolio-item-one__img" />
+        </a>
+        <h2 className="portfolio-item-two__title"> BrainFlix</h2>
+        <p className="portfolio-item-two__text">
+          BrainFlix is a mock youtube website. Built with dummy data in a server
+          with JSON data. I have then used Axios calls to select and switch
+          videos. You can also add new videos with a axios post form.
+        </p>
+        <div className="portfolio-logo_ctn"> </div>
+      </div>
+    </section>
+  );
+};
+
+export default Porfolio;
---
+++ src/resumeData.js
@@ -0,0 +1,109 @@
+let resumeData = {
+  imagebaseurl: "https://www.linkedin.com/in/mark-marley/",
+  name: "Mark Marley",
+  role: "Full-Stack Web Developer",
+  linkedinId: "",
+  // skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/mark-marley/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/MarkMarley-Dev",
+      className: "fa fa-github",
+    },
+    {
+      name: "skype",
+      url: "http://twitter.com/rbhatia46",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    " I recently graduated from Brainstation and currently working as a freelance Web Dev builing small scale websites for clients. I am a passionate learner with an eye for detail. When I'm not behind a computer screen I enjoy playing and watching football and having a spontaneous pint! ",
+  address: "Balham, London",
+  website: "07447821328",
+  education: [
+    {
+      UniversityName: "Brainstation",
+      specialization: "Full-Stack Web Development",
+      MonthOfPassing: "December ",
+      YearOfPassing: "2021",
+    },
+    {
+      UniversityName: "Team Treehouse",
+      specialization: "Web Dev online course",
+      MonthOfPassing: "August",
+      YearOfPassing: "2021",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Freelance Web Development",
+      specialization: "Reactjs,  JavaScript, Sass, Nodejs, HTML, CSS",
+      MonthOfLeaving: "December",
+      YearOfLeaving: "Onwards",
+      Achievements: "",
+    },
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "Nodejs",
+    },
+    {
+      skillname: "Sass",
+    },
+    {
+      skillname: "Chartjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Property Analyser",
+      description:
+        "A property Analysation tool to help property investors and homeowners alike gain access to granular data. ",
+      imgurl: "../../public/images/portfolio/propertyimg.png",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
